236dd258cbc5f1203a3aeaf259f3ce4da6cefe0d
Olga Polyakowa <polyakowa.o2011@yandex.ru>
2022-02-24 20:01:44 +0400

    feat: add solution part 2

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
+
   let arr = [...str];
   let keep = [];
-
-  if (arr.length % 2) return false; //work
-
-  for (let i = 0; i < bracketsConfig.length; i++) {
-    if (arr[0] == bracketsConfig[i][1]) return false;
-
-    if (arr[arr.length - 1] == bracketsConfig[i][0]) return false;
-  }
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < bracketsConfig.length; j++) {
       if (arr[i] == bracketsConfig[j][0]) {
-        keep.push(arr[i]);
+        if (
+          bracketsConfig[j][0] == bracketsConfig[j][1] &&
+          keep[keep.length - 1] == bracketsConfig[j][0]
+        ) {
+          keep.pop();
+        } else if (
+          bracketsConfig[j][0] == bracketsConfig[j][1] &&
+          keep[keep.length - 1] !== bracketsConfig[j][0]
+        ) {
+          keep.push(arr[i]);
+        } else {
+          keep.push(arr[i]);
+        }
       } else if (arr[i] == bracketsConfig[j][1]) {
-        let a = keep.pop();
-
-        if (a !== bracketsConfig[j][0]) return false;
+        if (keep.pop() !== bracketsConfig[j][0]) return false;
       }
     }
   }
